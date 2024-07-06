e99c3d5a86f65338f9ce65fdb220e37ac1ad7e27
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-07-07 00:24:05 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/yuya/Main.java
@@ -0,0 +1,17 @@
+package com.yuya;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか、
+// ガターで <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        List<String> humanlist = List.of("yuya", "taka", "yuta", "hiroki", "koudai");
+        System.out.println(humanlist.stream()
+                .map(String::toUpperCase)
+                .sorted()
+                .collect(Collectors.joining(".")));
+    }
+}
+
